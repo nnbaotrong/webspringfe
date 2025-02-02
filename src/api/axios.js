2762b7b7e139b3dcb251cbf679d5a06062e78a06
Trong Nguyen <nnbaotrong@gmail.com>
2025-02-02 13:00:59 +0700

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:8080/api', // URL API backend
+  // baseURL: 'http://localhost:8080/api', // URL API backend
+  baseURL: 'https://webspring.onrender.com/api', // URL API backend
 });
